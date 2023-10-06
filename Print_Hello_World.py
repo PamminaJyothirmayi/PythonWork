bef346e8892718c0f2620e16835d16cf8cbe9087
PamminaJyothirmayi <85344254+PamminaJyothirmayi@users.noreply.github.com>
2023-10-06 22:00:23 +0530

    Update Print_Hello_World.py

--- Print_Hello_World.py
+++ Print_Hello_World.py
@@ -1,13 +1,184 @@
-print("Hello World"); //Hello World
+print("Hello World"); #Hello World
 
-print(2+5)      //7
+print(2+5)        #7
 
-print("2 + 5")   //2+5
+print("2 + 5")   #2+5
 
 
-print(1 + 1.5)    //2.5
+print(1 + 1.5)    #2.5
 
-print(5 / 2)    //2.5
-print(4 / 2)    //2.0
+print(5 / 2)    #2.5
+print(4 / 2)    #2.0
+
+#Print three Hashes
+print("###");    """ ### """
+
+print(2495+789358)  #791853
+
+
+#Reads a line of input as a string and Take Input From User
+username = input()
+print(username)
+
+
+
+
+#String Concatenation
+a = "Hello" + " " + "World"
+print(a)
+
+#output : Hello World
+
+
+
+
+#String Repetition
+
+b = "*" * 10
+print(b)
+
+#output : **********
+
+
+
+
+
+s = "Python"
+s = ("* " * 3) + s + (" *" * 3)
+print(s)
+
+#* * * Python * * *
+
+
+
+#Length of String
+
+username = input()
+length = len(username)
+print(length)
+
+
+"""
+Jyothi
+6
+"""
+
+
+
+
+
+#String Indexing
+
+username = "Rani"
+first_letter = username[0]
+print(first_letter)
+
+#Output:  R
+
+
+
+
+#Print Name and Age
+a = input()
+b = input()
+print(a +" is "+b+" years old")
+
+"""
+Output:=
+Robert
+21
+Robert is 21 years old
+"""
+
+
+
+
+
+
+#String Repetition
+a = input()
+print((a+" ")*3)
+
+
+# Output : Apple Apple Apple 
+
+
+
+
+
+
+
+#First & Last Digits
+
+number = input()
+first_digit = number[0]
+last_digit = number[-1]
+
+print(first_digit)
+print(last_digit)
+
+
+
+"""
+1456
+1
+6
+"""
+
+
+
+
+
+#Reverse the digits
+
+n = input()
+print(n[::-1])
+
+"""
+123
+321
+"""
+
+
+
+
+
+#Index of Last Character
+a = input()
+print(len(a)-1)
+
+
+"""
+Python
+5
+"""
+
+
+
+#Half Length of String
+a = input()
+print(len(a)/2)
+
+"""
+Airplane
+4.0
+"""
+
+
+#Length Excluding Character 
+#print the length of the word excluding the first and last character.
+a = input()
+print(len(a)-2)
+
+"""
+Blockchain
+8
+"""
+
+
+
+
+
+
 
 
