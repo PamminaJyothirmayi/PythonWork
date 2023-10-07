16f324bc077a739a51e84f2365aeaa5bdf537c03
PamminaJyothirmayi <85344254+PamminaJyothirmayi@users.noreply.github.com>
2023-10-07 15:47:53 +0530

    Create Relational_and_Logical_Operator.py

---
+++ Relational_and_Logical_Operator.py
@@ -0,0 +1,44 @@
+#Zero or Positive or Negative
+num = int(input())
+
+if(num>0):
+    print("Positive")
+elif(num<0):
+    print("Negative")
+else:
+    print("Zero")
+
+
+
+#Greatest Among Three Numbers
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+if(n1>n2 and n1>n3):
+    print(n1)
+elif(n2>n1 and n2>n3):
+    print(n2)
+else:
+    print(n3)
+
+
+
+#Even or Odd Number
+num = int(input())
+remainder = n % 2
+if(remainder==0):
+    print("Even")
+else:
+    print("Odd")
+ 
+
+
+
+
+
+
+
+
+
+
