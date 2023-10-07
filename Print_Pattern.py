b0ddd8e3aeaf9bcd67e8154bd920918f4dd5c648
PamminaJyothirmayi <85344254+PamminaJyothirmayi@users.noreply.github.com>
2023-10-07 14:58:11 +0530

    Update Print_Pattern.py

--- Print_Pattern.py
+++ Print_Pattern.py
@@ -10,3 +10,33 @@ output : q*********0
 input : 9647329032
 output : 9********2
 '''
+
+
+
+# Print Star pattern
+
+n = int(input())
+for i in range(1,4):
+    print("* "*n)
+
+'''
+input : 4
+output :
+* * * * 
+* * * * 
+* * * * 
+'''
+
+#Print + pattern
+n = int(input())
+for i in range(1,4):
+    print("+ "*n)
+
+'''
+input : 4
+
+output:
++ + + +
++ + + +
++ + + +
+'''
