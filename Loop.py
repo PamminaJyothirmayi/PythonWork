e58e12601f795410ed3b5497908321a9c0d6a21a
PamminaJyothirmayi <85344254+PamminaJyothirmayi@users.noreply.github.com>
2023-10-07 17:59:13 +0530

    Update Loop.py

--- Loop.py
+++ Loop.py
@@ -1,4 +1,4 @@
-#Print N Times
+#Read N Times on N lines
 n = int(input())
 for i in range(n):
     print(0)
@@ -11,4 +11,38 @@ output:
 0
 '''
 
-coding practice 7A
+
+
+#First N Natural Numbers
+num = int(input())
+for i in range(1,num+1):
+    print(i)
+
+
+
+#Average of the N Numbers
+n = int(input())
+s=0
+j = 0
+for i in range(1,n+1):
+    s = s + i
+    j = j + 1
+avg = s / j 
+print(avg)
+
+
+
+
+#Cubes of N Numbers
+n = int(input())
+for i in range(1,n+1):
+    print(i**3)
+
+
+'''
+input : 4
+output : 1 8 27 64
+'''
+
+
+
